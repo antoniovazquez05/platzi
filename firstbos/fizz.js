6fc365fab084f1013d450e0a47729fe857f15f57
antoniovazquez05 <54754591+antoniovazquez05@users.noreply.github.com>
2019-08-31 17:20:41 -0500

    Add files via upload

---
+++ firstbos/fizz.js
@@ -0,0 +1,34 @@
+var numero = 100;
+for(var i = 1; i <= 100; i++)
+{
+
+    if(i % 3 == 0)
+
+    {
+      document.write("fizz");
+    }
+    if(i % 5 ==0)
+      {
+
+            document.write("buzz");
+
+        }
+
+        if(i % 3 != 0 && i % 5 !=0)
+
+  document.write(i);
+
+document.write("<br />");
+}
+
+ function esDivisible(num, divisor)
+{
+  if(num % divisor == 0)
+{
+  return true;
+}
+else
+{
+  return false;
+}
+}
